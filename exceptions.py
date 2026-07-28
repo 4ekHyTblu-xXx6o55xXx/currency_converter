6f6e8ac4eb3b45cec5f9e95495111a8a41f66541
Ivan <levchenkoivan3007@gmail.com>
2026-07-28 13:31:33 +1000

    Delete exceptions.py

--- exceptions.py
+++
@@ -1,8 +0,0 @@
-class CurrencyError(Exception):
-    """Главная ошибка проекта"""
-
-class APIError(CurrencyError):
-    """Ошибка при обращении к сайту"""
-
-class CurrencyNotFoundError(CurrencyError):
-    """Отсутствие данной валюты"""
