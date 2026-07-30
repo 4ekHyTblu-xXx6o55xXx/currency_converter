82c32207667de9b6b5483c1395aa52cbde3cc3a0
Ivan <levchenkoivan3007@gmail.com>
2026-07-30 23:49:12 +1000

    Update cache.py

--- currency-converter/cache.py
+++ currency-converter/cache.py
@@ -3,19 +3,19 @@ import time
 class Cache:
     def __init__(self, ttl_seconds=3600):
         self._data = {}
-        self._times = {}
+        self._timestamps = {}
+        self._ttl = ttl_seconds
 
     def get(self, key):
         if key in self._data:
-            age = time.time() - self._times[key]
-            if age < self._ttl_seconds:
+            age = time.time() - self._timestamps[key]
+            if age < self._ttl:
                 return self._data[key]
             else:
                 del self._data[key]
-                del self._times[key]
+                del self._timestamps[key]
         return None
 
     def set(self, key, value):
         self._data[key] = value
-        self._times[key] = time.time()
-    
+        self._timestamps[key] = time.time()
